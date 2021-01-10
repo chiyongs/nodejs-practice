5d8afe07cd035214a2804c9fd8dfea3d12200628
chiyongs <tlscldyd@gmail.com>
2021-01-11 00:17:56 +0900

    Add path create

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const qs = require("querystring");
 
 const templateList = (list) => {
   let i = 0;
@@ -22,6 +23,7 @@ const templateHTML = (title, content, body) => {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${content}
+    <a href="/create">Create</a>
     ${body}
   </body>
   </html>
@@ -64,6 +66,36 @@ const server = http.createServer(function (request, response) {
         });
       });
     }
+  } else if (PATHNAME === "/create") {
+    fs.readdir("./data", (err, list) => {
+      const content = templateList(list);
+      const title = "Web - Create";
+      const template = templateHTML(
+        title,
+        content,
+        `<form action="http://localhost:3000/create_process" method=POST>
+        <p><input type = "text" placeholder="title" name="title"></p>
+        <p><textarea name="content" placeholder="content"></textarea></p>
+        <input type = "submit">
+        </form>`
+      );
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (PATHNAME === "/create_process") {
+    if (request.method == "POST") {
+      let body = "";
+      request.on("data", (data) => {
+        body += data;
+      });
+      request.on("end", (data) => {
+        const post = qs.parse(body);
+        const title = post.title;
+        const content = post.content;
+      });
+    }
+    response.writeHead(200);
+    response.end("success");
   } else {
     response.writeHead(404);
     response.end("Not Found!");
