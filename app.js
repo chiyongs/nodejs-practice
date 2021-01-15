6f1c418f293bac9ae1089bb69ecd194da4c13b58
chiyongs <tlscldyd@gmail.com>
2021-01-16 05:51:50 +0900

    Routing pages

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const sanitizeHtml = require("sanitize-html");
 const template = require("./lib/template");
 const bodyParser = require("body-parser");
 const compression = require("compression");
+const pageRouter = require("./routes/page");
 const app = express();
 const port = 3000;
 
@@ -18,6 +19,8 @@ app.get("*", (req, res, next) => {
   });
 });
 
+app.use("/page", pageRouter);
+
 //route, routing
 app.get("/", (req, res) => {
   const title = "Welcome";
@@ -29,103 +32,20 @@ app.get("/", (req, res) => {
     `<h2>${title}</h2>
       <p>${data}</p>
       <img src="/images/7.jpg" style="width:300px; display:block"/>`,
-    `<a href="/create">Create</a>`
+    `<a href="/page/create">Create</a>`
   );
   res.writeHead(200);
   res.end(html);
 });
 
-app.get("/page/:pageId", (req, res) => {
-  const filteredID = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredID}`, "utf8", (err, data) => {
-    const title = req.params.pageId;
-    const sanitizedTitle = sanitizeHtml(title);
-    const sanitizedData = sanitizeHtml(data);
-    const list = template.List(req.list);
-    const html = template.HTML(
-      sanitizedTitle,
-      list,
-      `<h2>${sanitizedTitle}</h2>
-      <p>${sanitizedData}</p>`,
-      `<a href="/create">Create</a> <a href="/update/${sanitizedTitle}">Update</a>
-        <form action="/delete_process" method = "POST" >
-        <input type="hidden" name="id" value="${sanitizedTitle}">
-        <input type="submit" value="delete">
-        </form>`
-    );
-    res.send(html);
-  });
-});
-
-app.get("/create", (req, res) => {
-  const title = "Web - Create";
-  const list = template.List(req.list);
-  const html = template.HTML(
-    title,
-    list,
-    `<form action="/create_process" method=POST>
-          <p><input type = "text" placeholder="title" name="title"></p>
-          <p><textarea name="content" placeholder="content"></textarea></p>
-          <input type = "submit">
-          </form>`,
-    ""
-  );
-  res.send(html);
-});
-
-app.post("/create_process", (req, res) => {
-  const post = req.body;
-  const title = post.title;
-  const content = post.content;
-  fs.writeFile(`data/${title}`, content, "utf8", (err) => {
-    res.redirect(`page/${title}`);
-  });
-});
-
-app.get("/update/:pageId", (req, res) => {
-  const filteredID = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredID}`, "utf8", (err, data) => {
-    const title = req.params.pageId;
-    const list = template.List(req.list);
-    const html = template.HTML(
-      title,
-      list,
-      `<form action="/update_process" method=POST>
-            <input type = "hidden" name="id" value="${title}"}>
-          <p><input type = "text" placeholder="title" name="title" value="${title}"></p>
-          <p><textarea name="content" placeholder="content">${data}</textarea></p>
-          <input type = "submit">
-          </form>`,
-      ""
-    );
-    res.send(html);
-  });
-});
-
-app.post("/update_process", (req, res) => {
-  const post = req.body;
-  const id = post.id;
-  const title = post.title;
-  const content = post.content;
-  fs.rename(`data/${id}`, `data/${title}`, (err) => {
-    fs.writeFile(`data/${title}`, content, "utf8", (err) => {
-      res.redirect(`page/${title}`);
-    });
-  });
-});
-
-app.post("/delete_process", (req, res) => {
-  const post = req.body;
-  const id = post.id;
-  const filteredID = path.parse(id).base;
-  fs.unlink(`data/${filteredID}`, (err) => {
-    res.redirect("/");
-  });
-});
-
 // 위의 함수들을 다 실행한 후에 라우팅에 맞는 함수가 없다는 것으로 판단되어서 매칭되는 것이 없으므로 404 error
 app.use(function (req, res, next) {
   res.status(404).send("Sorry cant find that!");
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 app.listen(port, () => {
---
+++ routes/page.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const fs = require("fs");
+const sanitizeHtml = require("sanitize-html");
+const template = require("../lib/template");
+const bodyParser = require("body-parser");
+const compression = require("compression");
+
+router.get("/create", (req, res) => {
+  const title = "Web - Create";
+  const list = template.List(req.list);
+  const html = template.HTML(
+    title,
+    list,
+    `<form action="/page/create_process" method=POST>
+            <p><input type = "text" placeholder="title" name="title"></p>
+            <p><textarea name="content" placeholder="content"></textarea></p>
+            <input type = "submit">
+            </form>`,
+    ""
+  );
+  res.send(html);
+});
+
+router.post("/create_process", (req, res) => {
+  const post = req.body;
+  const title = post.title;
+  const content = post.content;
+  fs.writeFile(`data/${title}`, content, "utf8", (err) => {
+    res.redirect(`/page/${title}`);
+  });
+});
+
+router.get("/update/:pageId", (req, res) => {
+  const filteredID = path.parse(req.params.pageId).base;
+  fs.readFile(`data/${filteredID}`, "utf8", (err, data) => {
+    const title = req.params.pageId;
+    const list = template.List(req.list);
+    const html = template.HTML(
+      title,
+      list,
+      `<form action="/page/update_process" method=POST>
+              <input type = "hidden" name="id" value="${title}"}>
+            <p><input type = "text" placeholder="title" name="title" value="${title}"></p>
+            <p><textarea name="content" placeholder="content">${data}</textarea></p>
+            <input type = "submit">
+            </form>`,
+      ""
+    );
+    res.send(html);
+  });
+});
+
+router.post("/update_process", (req, res) => {
+  const post = req.body;
+  const id = post.id;
+  const title = post.title;
+  const content = post.content;
+  fs.rename(`data/${id}`, `data/${title}`, (err) => {
+    fs.writeFile(`data/${title}`, content, "utf8", (err) => {
+      res.redirect(`/page/${title}`);
+    });
+  });
+});
+
+router.post("/delete_process", (req, res) => {
+  const post = req.body;
+  const id = post.id;
+  const filteredID = path.parse(id).base;
+  fs.unlink(`data/${filteredID}`, (err) => {
+    res.redirect("/");
+  });
+});
+
+router.get("/:pageId", (req, res, next) => {
+  const filteredID = path.parse(req.params.pageId).base;
+  fs.readFile(`data/${filteredID}`, "utf8", (err, data) => {
+    if (err) {
+      next(err);
+    } else {
+      const title = req.params.pageId;
+      const sanitizedTitle = sanitizeHtml(title);
+      const sanitizedData = sanitizeHtml(data);
+      const list = template.List(req.list);
+      const html = template.HTML(
+        sanitizedTitle,
+        list,
+        `<h2>${sanitizedTitle}</h2>
+          <p>${sanitizedData}</p>`,
+        `<a href="/page/create">Create</a> <a href="/page/update/${sanitizedTitle}">Update</a>
+            <form action="/page/delete_process" method = "POST" >
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+            </form>`
+      );
+      res.send(html);
+    }
+  });
+});
+
+module.exports = router;
